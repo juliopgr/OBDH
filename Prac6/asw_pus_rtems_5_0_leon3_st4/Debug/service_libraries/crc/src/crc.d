82af5f364ed1e6feb7c8ab28c89bda2482597048
juliopgr <julitodo2005@gmail.com>
2026-05-01 14:56:04 +0200

    Prac7 copiada como base de la prac6

---
+++ Prac6/asw_pus_rtems_5_0_leon3_st4/Debug/service_libraries/crc/src/crc.d
@@ -0,0 +1,5 @@
+service_libraries/crc/src/crc.o: ../service_libraries/crc/src/crc.c \
+ /home/atcsol/git/OBDH/Prac7/asw_pus_rtems_5_0_leon3_st19/service_libraries/crc/include/public/crc.h \
+ /home/atcsol/git/OBDH/Prac7/asw_pus_rtems_5_0_leon3_st19/llsw/config/include/public/basic_types.h
+/home/atcsol/git/OBDH/Prac7/asw_pus_rtems_5_0_leon3_st19/service_libraries/crc/include/public/crc.h:
+/home/atcsol/git/OBDH/Prac7/asw_pus_rtems_5_0_leon3_st19/llsw/config/include/public/basic_types.h:
