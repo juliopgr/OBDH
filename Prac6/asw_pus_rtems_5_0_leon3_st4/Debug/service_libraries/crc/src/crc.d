17c997c1f76145b7778ccce62589c0dff1e5500f
juliopgr <158303997+juliopgr@users.noreply.github.com>
2026-05-01 14:51:17 +0200

    Borron y cuenta nueva prac6

--- Prac6/asw_pus_rtems_5_0_leon3_st4/Debug/service_libraries/crc/src/crc.d
+++
@@ -1,5 +0,0 @@
-service_libraries/crc/src/crc.o: ../service_libraries/crc/src/crc.c \
- /home/atcsol/git/OBDH/Prac5/asw_pus_rtems_5_0_leon3_st5_st12/service_libraries/crc/include/public/crc.h \
- /home/atcsol/git/OBDH/Prac5/asw_pus_rtems_5_0_leon3_st5_st12/llsw/config/include/public/basic_types.h
-/home/atcsol/git/OBDH/Prac5/asw_pus_rtems_5_0_leon3_st5_st12/service_libraries/crc/include/public/crc.h:
-/home/atcsol/git/OBDH/Prac5/asw_pus_rtems_5_0_leon3_st5_st12/llsw/config/include/public/basic_types.h:
